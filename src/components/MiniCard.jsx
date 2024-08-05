f69366c93994f86478f5414193d91559730bb98e
ShreyaKurry13 <109572940+ShreyaKurry13@users.noreply.github.com>
2024-08-06 01:49:08 +0530

    code changes:[updated]

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -1,45 +1,43 @@
-// /* eslint-disable */
+// /* eslint-disable react/prop-types */
 // import React, { useEffect, useState } from 'react'
-
 // import sun from '../assets/icons/sun.png'
 // import cloud from '../assets/icons/cloud.png'
+// import fog from '../assets/icons/fog.png'
 // import rain from '../assets/icons/rain.png'
 // import snow from '../assets/icons/snow.png'
-// import fog from '../assets/icons/fog.png'
 // import storm from '../assets/icons/storm.png'
-// import windy from '../assets/icons/windy.png'
+// import wind from '../assets/icons/windy.png'
 
-// const MiniCard = ({time, temp, iconString}) => {
+// const MiniCard = ({ time, temp, iconString }) => {
 //   const [icon, setIcon] = useState()
 
-//     useEffect (() => {
-//       if (iconString) {
-//         if (iconString.toLowerCase().includes('cloud')) {
-//           setIcon(cloud)
-//         }else if(iconString.toLowerCase().includes('rain')){
-//           setIcon(rain)
-//         }else if(iconString.toLowerCase().includes('clear')){
-//           setIcon(sun)
-//         }else if(iconString.toLowerCase().includes('thunder')){
-//           setIcon(storm)
-//         }else if(iconString.toLowerCase().includes('fog')){
-//           setIcon(fog)
-//         }else if(iconString.toLowerCase().includes('snow')){
-//           setIcon(snow)
-//         }else if(iconString.toLowerCase().includes('wind')){
-//           setIcon(windy)
-//         }
+//   useEffect(() => {
+//     if (iconString) {
+//       if (iconString.toLowerCase().includes('cloud')) {
+//         setIcon(cloud)
+//       } else if (iconString.toLowerCase().includes('rain')) {
+//         setIcon(rain)
+//       } else if (iconString.toLowerCase().includes('clear')) {
+//         setIcon(sun)
+//       } else if (iconString.toLowerCase().includes('thunder')) {
+//         setIcon(storm)
+//       } else if (iconString.toLowerCase().includes('fog')) {
+//         setIcon(fog)
+//       } else if (iconString.toLowerCase().includes('snow')) {
+//         setIcon(snow)
+//       } else if (iconString.toLowerCase().includes('wind')) {
+//         setIcon(wind)
 //       }
-//   },[iconString])
-
+//     }
+//   }, [iconString])
 //   return (
 //     <div className='glassCard w-[10rem] h-[10rem] p-4 flex flex-col'>
-//       <p className=' text-center'>
-//         {new Date(time).toLocaleTimeString('en',{weekday: 'long'}).split(" ")[0]}
+//       <p className='text-center'>
+//         {new Date(time).toLocaleTimeString('en', { weekday: 'long' }).split(" ")[0]}
 //       </p>
 //       <hr />
-//       <div className=' w-full flex justify-center items-center flex-1'>
-//         <img src={icon} alt="forecast not available()" className=' w-[4rem] h-[4rem]' />
+//       <div className='w-full flex justify-center items-center flex-1'>
+//         <img src={icon} alt="forecast not available" className='w-[4rem] h-[4rem]' />
 //       </div>
 //       <p className='text-center font-bold'>{temp}&deg;C</p>
 //     </div>
@@ -48,22 +46,21 @@
 
 // export default MiniCard
 
-// /* eslint-enable */
 
 
-/* eslint-disable */
+
+/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react'
-
 import sun from '../assets/icons/sun.png'
 import cloud from '../assets/icons/cloud.png'
+import fog from '../assets/icons/fog.png'
 import rain from '../assets/icons/rain.png'
 import snow from '../assets/icons/snow.png'
-import fog from '../assets/icons/fog.png'
 import storm from '../assets/icons/storm.png'
-import windy from '../assets/icons/windy.png'
+import wind from '../assets/icons/windy.png'
 
 const MiniCard = ({ time, temp, iconString }) => {
-  const [icon, setIcon] = useState(null)
+  const [icon, setIcon] = useState()
 
   useEffect(() => {
     if (typeof iconString === 'string') {
@@ -81,24 +78,23 @@ const MiniCard = ({ time, temp, iconString }) => {
       } else if (lowerCaseIconString.includes('snow')) {
         setIcon(snow)
       } else if (lowerCaseIconString.includes('wind')) {
-        setIcon(windy)
+        setIcon(wind)
       }
     }
   }, [iconString])
 
   return (
     <div className='glassCard w-[10rem] h-[10rem] p-4 flex flex-col'>
-      <p className=' text-center'>
+      <div className='text-center'>
         {new Date(time).toLocaleTimeString('en', { weekday: 'long' }).split(" ")[0]}
-      </p>
-      <hr />
-      <div className=' w-full flex justify-center items-center flex-1'>
-        <img src={icon} alt="forecast not available()" className=' w-[4rem] h-[4rem]' />
       </div>
-      <p className='text-center font-bold'>{temp}&deg;C</p>
+      <hr />
+      <div className='w-full flex justify-center items-center flex-1'>
+        <img src={icon} alt="forecast not available" className='w-[4rem] h-[4rem]' />
+      </div>
+      <div className='text-center font-bold'>{temp}&deg;C</div>
     </div>
   )
 }
 
 export default MiniCard
-/* eslint-enable */
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,13 +1,94 @@
+// /* eslint-disable react/prop-types */
+// import React, { useEffect, useState } from 'react'
+// import { useDate } from '../Utils/useData'
+
+
+// import sun from '../assets/icons/sun.png'
+// import cloud from '../assets/icons/cloud.png'
+// import fog from '../assets/icons/fog.png'
+// import rain from '../assets/icons/rain.png'
+// import snow from '../assets/icons/snow.png'
+// import storm from '../assets/icons/storm.png'
+// import wind from '../assets/icons/windy.png'
+// import '../index.css'
+
+// const WeatherCard = ({
+//   temperature,
+//   windspeed,
+//   humidity,
+//   place,
+//   heatIndex,
+//   iconString,
+//   conditions,
+// }) => {
+
+//   const [icon, setIcon] = useState(sun)
+//   const { time } = useDate()
+
+//   useEffect(() => {
+//     if (iconString) {
+//       if (iconString.toLowerCase().includes('cloud')) {
+//         setIcon(cloud)
+//       } else if (iconString.toLowerCase().includes('rain')) {
+//         setIcon(rain)
+//       } else if (iconString.toLowerCase().includes('clear')) {
+//         setIcon(sun)
+//       } else if (iconString.toLowerCase().includes('thunder')) {
+//         setIcon(storm)
+//       } else if (iconString.toLowerCase().includes('fog')) {
+//         setIcon(fog)
+//       } else if (iconString.toLowerCase().includes('snow')) {
+//         setIcon(snow)
+//       } else if (iconString.toLowerCase().includes('wind')) {
+//         setIcon(wind)
+//       }
+//     }
+//   }, [iconString])
+
+//   return (
+//     <div className='w-[22rem] min-w-[22rem] h-[30rem] glassCard p-4'>
+//       <div className='flex w-full just-center, items-center gap-4 mt-12 mb-4'>
+//         <img src={icon} alt="weather_icon" />
+//         <p className='font-bold text-5xl flex justify-center items-center' >{temperature} &deg;C</p>
+//       </div>
+//       <div className='font-bold text-center text-xl'>
+//         {place}
+//       </div>
+//       <div className='w-full flex justify-between items-center mt-4'>
+//         <p className='flex-1 text-center p-2'>{new Date().toDateString()}</p>
+//         <p className='flex-1 text-center p-2'>{time}</p>
+//       </div>
+//       <div className='w-full flex justify-between items-center mt-4 gap-4'>
+//         <p className='flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg'>Wind Speed <p className='font-normal'>{windspeed} km/h</p></p>
+//         <p className='flex-1 text-center p-2 font-bold rounded-lg bg-green-600'>Humidity <p className='font-normal'>{humidity} gm/m&#179;</p></p>
+//       </div>
+//       <div className='w-full p-3 mt-4 flex justify-between items-center'>
+//         <p className='font-semibold text-lg'>Heat Index</p>
+//         <p className='text-lg'>{heatIndex ? heatIndex : 'N/A'}</p>
+//       </div>
+//       <hr className='bg-slate-600' />
+//       <div className='w-full p-4 flex justify-center items-center text-3xl font-semibold'>
+//         {conditions}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default WeatherCard
+
+
+
+/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react'
-import {useDate} from '../Utils/useData'
+import { useDate } from '../Utils/useData'
 
 import sun from '../assets/icons/sun.png'
 import cloud from '../assets/icons/cloud.png'
+import fog from '../assets/icons/fog.png'
 import rain from '../assets/icons/rain.png'
 import snow from '../assets/icons/snow.png'
-import fog from '../assets/icons/fog.png'
 import storm from '../assets/icons/storm.png'
-import windy from '../assets/icons/windy.png'
+import wind from '../assets/icons/windy.png'
 import '../index.css'
 
 const WeatherCard = ({
@@ -19,54 +100,60 @@ const WeatherCard = ({
   iconString,
   conditions,
 }) => {
- 
-  const[icon, setIcon] = useState(sun)
-  const {time} = useDate()
- 
-  useEffect(() =>{
-  if (iconString) {
-    if (iconString.toLowerCase().includes('cloud')) {
-      setIcon(cloud)
-    }else if(iconString.toLowerCase().includes('rain')){
-      setIcon(rain)
-    }else if(iconString.toLowerCase().includes('clear')){
-      setIcon(sun)
-    }else if(iconString.toLowerCase().includes('thunder')){
-      setIcon(storm)
-    }else if(iconString.toLowerCase().includes('fog')){
-      setIcon(fog)
-    }else if(iconString.toLowerCase().includes('snow')){
-      setIcon(snow)
-    }else if(iconString.toLowerCase().includes('wind')){
-      setIcon(windy)
+
+  const [icon, setIcon] = useState(sun)
+  const { time } = useDate()
+
+  useEffect(() => {
+    if (iconString) {
+      if (iconString.toLowerCase().includes('cloud')) {
+        setIcon(cloud)
+      } else if (iconString.toLowerCase().includes('rain')) {
+        setIcon(rain)
+      } else if (iconString.toLowerCase().includes('clear')) {
+        setIcon(sun)
+      } else if (iconString.toLowerCase().includes('thunder')) {
+        setIcon(storm)
+      } else if (iconString.toLowerCase().includes('fog')) {
+        setIcon(fog)
+      } else if (iconString.toLowerCase().includes('snow')) {
+        setIcon(snow)
+      } else if (iconString.toLowerCase().includes('wind')) {
+        setIcon(wind)
+      }
     }
-  }
-  },[iconString])
+  }, [iconString])
 
   return (
     <div className='w-[22rem] min-w-[22rem] h-[30rem] glassCard p-4'>
-      <div className='flex w-full justify-center, items-center gap-4 mt-12 mb-4'>
-       <img src={icon} alt='weather_icon' />
-       <p className='font-bold text-5xl flex justify-center items-center'>{temperature} &deg;C</p>
+      <div className='flex w-full justify-center items-center gap-4 mt-12 mb-4'>
+        <img src={icon} alt="weather_icon" />
+        <p className='font-bold text-5xl flex justify-center items-center'>{temperature} &deg;C</p>
       </div>
       <div className='font-bold text-center text-xl'>
         {place}
       </div>
       <div className='w-full flex justify-between items-center mt-4'>
-          <p className='flex-1 text-center p-2'>{new Date().toDateString()}</p>
-          <p className='flex-1 text-center p-2'>{time}</p>
+        <div className='flex-1 text-center p-2'>{new Date().toDateString()}</div>
+        <div className='flex-1 text-center p-2'>{time}</div>
       </div>
       <div className='w-full flex justify-between items-center mt-4 gap-4'>
-          <p className='flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg'>Wind Speed <p className='font-normal'>{windspeed} km/h</p></p>
-          <p className='flex-1 text-center p-2 font-bold rounded-lg bg-green-600'>Humidity <p className='font-normal'>{humidity} gm/m&#179;</p></p>
+        <div className='flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg'>
+          Wind Speed
+          <div className='font-normal'>{windspeed} km/h</div>
+        </div>
+        <div className='flex-1 text-center p-2 font-bold rounded-lg bg-green-600'>
+          Humidity
+          <div className='font-normal'>{humidity} gm/m&#179;</div>
+        </div>
       </div>
       <div className='w-full p-3 mt-4 flex justify-between items-center'>
-           <p className='font-semibold text-lg'>Heat Index</p>
-           <p className='text-lg'>{heatIndex?heatIndex : 'N/A'}</p>
+        <div className='font-semibold text-lg'>Heat Index</div>
+        <div className='text-lg'>{heatIndex ? heatIndex : 'N/A'}</div>
       </div>
-      <hr className=' bg-slate-600'/>
-      <div className=' w-full p-4 flex justify-center items-center text-3xl font-semibold'>
-         {conditions}
+      <hr className='bg-slate-600' />
+      <div className='w-full p-4 flex justify-center items-center text-3xl font-semibold'>
+        {conditions}
       </div>
     </div>
   )
